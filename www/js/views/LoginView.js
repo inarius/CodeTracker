17232878e65328a70d2e84cb0cec1dd54a1bc1bf
simonej <jsimone@gmail.com>
2014-05-04 17:53:04 -0700

    add GPS test

--- www/js/views/LoginView.js
+++ www/js/views/LoginView.js
@@ -20,7 +20,32 @@ app.views.LoginView = Backbone.View.extend({
     render: function () {
         this.$el.html(this.template());
         this.bindDOM();
+        this.getGPS();
         return this;
+    },
+
+    getGPS: function () {
+        // onSuccess Callback
+        // This method accepts a Position object, which contains the
+        // current GPS coordinates
+        //
+        var onSuccess = function (position) {
+            this.$el.html('Latitude: ' + position.coords.latitude + '\n' +
+                          'Longitude: ' + position.coords.longitude + '\n' +
+                          'Altitude: ' + position.coords.altitude + '\n' +
+                          'Accuracy: ' + position.coords.accuracy + '\n' +
+                          'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+                          'Heading: ' + position.coords.heading + '\n' +
+                          'Speed: ' + position.coords.speed + '\n' +
+                          'Timestamp: ' + position.timestamp + '\n');
+        };
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: ' + error.code + '\n' +
+                  'message: ' + error.message + '\n');
+        }
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     },
 
     bindDOM: function () {
